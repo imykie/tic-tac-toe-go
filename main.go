728fd70fabc74c66e0bcd0daeb8251b75f82f403
Michael Olatunji <tundexmike@gmail.com>
2021-06-12 20:27:37 +0100

    formatting

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	board := [9]int{0,0,0,0,0,0,0,0,0}
+	board := [9]int{0, 0, 0, 0, 0, 0, 0, 0, 0}
 	gameOver := false
 	turn := 1
 
@@ -14,7 +14,7 @@ func main() {
 	for gameOver != true {
 		displayBoard(board)
 		player := 0
-		if turn % 2 == 1 {
+		if turn%2 == 1 {
 			fmt.Println("Player 1's turn")
 			player = 1
 		} else {
@@ -46,7 +46,7 @@ func main() {
 }
 
 func showInstructions() {
-	board := [9]int{2,2,2,2,2,2,2,2,2}
+	board := [9]int{2, 2, 2, 2, 2, 2, 2, 2, 2}
 	fmt.Println("Each digit in the boxes represent their play numbers")
 	displayBoard(board)
 	fmt.Printf("\n\n")
@@ -64,7 +64,7 @@ func displayBoard(board [9]int) {
 			fmt.Printf(" %d ", i)
 		}
 
-		if i > 0 && (i + 1) % 3 == 0 {
+		if i > 0 && (i+1)%3 == 0 {
 			fmt.Printf("\n")
 		} else {
 			fmt.Printf("|")
@@ -80,21 +80,21 @@ func askToPlay() int {
 }
 
 func executeMove(move, player int, board [9]int) [9]int {
-	 if board[move] != 0 {
+	if board[move] != 0 {
 		fmt.Println("Please pick an available box")
-		move =  askToPlay()
+		move = askToPlay()
 		board = executeMove(move, player, board)
-	 } else {
+	} else {
 		if player == 1 {
 			board[move] = 1
 		} else if player == 2 {
 			board[move] = 10
 		}
-	 }
+	}
 
- 	for move > 9 {
- 		fmt.Println("Please enter a number less than 10")
- 		move = askToPlay()
+	for move > 9 {
+		fmt.Println("Please enter a number less than 10")
+		move = askToPlay()
 	}
 
 	if player == 1 {
@@ -107,20 +107,20 @@ func executeMove(move, player int, board [9]int) [9]int {
 }
 
 func checkForWin(b [9]int) int {
-	sums := [8] int {0,0,0,0,0,0,0,0}
-	sums[0] = b[2]+b[4]+b[6]
-	sums[1] = b[0]+b[3]+b[6]
-	sums[2] = b[1]+b[4]+b[7]
-	sums[3] = b[2]+b[5]+b[8]
-	sums[4] = b[0]+b[4]+b[8]
-	sums[5] = b[6]+b[7]+b[8]
-	sums[6] = b[3]+b[4]+b[5]
-	sums[7] = b[0]+b[1]+b[2]
+	sums := [8]int{0, 0, 0, 0, 0, 0, 0, 0}
+	sums[0] = b[2] + b[4] + b[6]
+	sums[1] = b[0] + b[3] + b[6]
+	sums[2] = b[1] + b[4] + b[7]
+	sums[3] = b[2] + b[5] + b[8]
+	sums[4] = b[0] + b[4] + b[8]
+	sums[5] = b[6] + b[7] + b[8]
+	sums[6] = b[3] + b[4] + b[5]
+	sums[7] = b[0] + b[1] + b[2]
 
 	for _, v := range sums {
 		if v == 3 {
 			return 1
-		} else if v == 30{
+		} else if v == 30 {
 			return 2
 		}
 	}
